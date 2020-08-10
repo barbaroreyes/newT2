1c62d1c96617d0a344d85dd9ecaf5a100967aecc
barbarof <barbarofreyes@gmail.com>
2020-08-10 12:06:35 -0500

    added infromation

---
+++ src/Componnt/Card.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Card =  ({id,name,email}) => {
+    return (
+        <div>
+        <img alt ='photo ' src = {`https://robohash.org/${id}?200x200`} />
+        
+         <div>
+         <h1>{name}</h1>
+         <p>{email}</p>
+         </div>
+        </div>
+    )
+
+}
+export default Card 
