419ea2c8739a7de70e1bf4c73530228f0ae21b86
barbarof <barbarofreyes@gmail.com>
2020-08-11 09:54:28 -0500

    added  map

--- src/Componnt/CarList.js
+++ src/Componnt/CarList.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import Card from './Card';
-import robots from './robots'
- const CardList = () => {
-   
-         return (<div>
-            <Card id={robots[0].id} name ={robots[0].name} email={robots[0].e}/>
-            <Card id={robots[1].id} name ={robots[1].name} email={robots[1].e}/>
-            <Card id={robots[2].id} name ={robots[2].name} email={robots[2].e}/>
+
+ const CardList = ({robots}) => {
+     const cardCompo = robots.map((user , i) => {
+         return (
+            <Card key={i} id={robots[i].id} name ={robots[i].name} email={robots[i].email}/>   
+         )
+     })
+       return (<div>
+              {cardCompo}
            </div>)
-     
  }
  export default CardList
 
